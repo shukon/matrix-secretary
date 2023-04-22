332e09f0b46a3750af5ca52775497cf7e0e00175
shukon <>
2023-04-22 18:23:15 +0200

    Maint fallback translation

--- secretary/translations.py
+++ secretary/translations.py
@@ -1,7 +1,7 @@
 translations = {
     'helptext': {
         'en': "Hi, I'm here to help you manage your bots and create complex rooms settings for you.",
-        'de': "Hallo, ich bin hier, um dir zu helfen, deine Bots zu verwalten und komplexe Raum-Einstellungen für dich zu erstellen.",
+        'de': "Hallo, ich bin hier, um deine Bots zu verwalten und komplexe Raum-Einstellungen für dich zu erstellen.",
     },
     'generic_error': {
         'en': "Sorry, I tried, but I encountered an error. Please check the logs.",
@@ -11,4 +11,8 @@ translations = {
 
 
 def echo(phrase, lang):
+    if phrase not in translations:
+        return "Phrase not found: " + phrase
+    if lang not in translations[phrase]:
+        return translations[phrase]['en']
     return translations[phrase][lang]
